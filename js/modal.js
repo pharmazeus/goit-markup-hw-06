8d68ac63a03cebed44e9ddada6495b9596355ff5
Pharma.zeus <markmaydeens@gmail.com>
2025-01-21 00:48:52 -0500

    post-fix-v1

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+//Modal Window
 const orderServiceAndModalBtn = document.querySelector(".order-service-btn");
 const modal = document.querySelector(".backdrop");
 const closeModalBtn = document.querySelector(".close-btn");
@@ -18,3 +19,18 @@ modal.addEventListener("click", (event) => {
 
 closeModalBtn.addEventListener("click", closeModalWindow);
 orderServiceAndModalBtn.addEventListener("click", openModalWindow);
+//End Modal Window
+//Mobile Menu
+const mobileMenu = document.querySelector(".mobile-menu");
+const openMobileMenuBtn = document.querySelector(".menu-head-btn");
+const closeMobileMenuBtn = document.querySelector(".mobile-close-btn");
+
+const toOpenMobileMenu = () => {
+  mobileMenu.classList.add("is-open");
+};
+const toCloseMobileMenu = () => {
+  mobileMenu.classList.remove("is-open");
+};
+
+closeMobileMenuBtn.addEventListener("click", toCloseMobileMenu);
+openMobileMenuBtn.addEventListener("click", toOpenMobileMenu);
